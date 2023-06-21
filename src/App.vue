1c8b694309dab39f9c2985432c2d91426eccd399
nickwasused <github.lix1s@simplelogin.co>
2023-06-21 14:03:03 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -19,8 +19,8 @@ const gql_error = ref(null)
 const gql_timer = ref<number | null>(null)
 const QUERY = `
 query {
-    getViewerCount(title: "a")
-    Streamers(title: "a") {
+    getViewerCount(title: "luckyv,lucky v")
+    Streamers(title: "luckyv,lucky v") {
       user_id
       user_name
       title
