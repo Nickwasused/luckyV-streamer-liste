0c30d5b455e49d3901a0b70e5e77403db42c7813
nickwasused <github.lix1s@simplelogin.co>
2023-08-19 15:31:59 +0200

    Update StreamerItem.vue

--- src/components/StreamerItem.vue
+++ src/components/StreamerItem.vue
@@ -40,7 +40,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from "vue"
+import { computed } from "vue"
 import { useI18n } from "vue-i18n"
 import TwitchImage from "./TwitchImage.vue"
 
